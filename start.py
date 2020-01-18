aaaa702c980aeed5148675e5267bb270a4b22f95
zcp159 <330116856@qq.com>
2020-01-18 23:02:27 +0800

    增加次数阈值功能

--- start.py
+++ start.py
@@ -3,9 +3,13 @@ import os
 import fnmatch
 import time
 import configparser
+import math
 
+# --------------------配置文件-------------------------
+# --------------------配置文件-------------------------
+# --------------------配置文件-------------------------
 
-# 导入章节,返回选项内容列表
+# 导入配置文件章节,返回选项内容列表
 def xuanxiang_read(cf, zhangjie):
     list = []
     # 遍历选项
@@ -14,7 +18,7 @@ def xuanxiang_read(cf, zhangjie):
     return list
 
 
-# 返回元组
+# 返回配置文件元组
 def conf_read():
     list = []
     cf = configparser.ConfigParser(allow_no_value=False)
@@ -28,73 +32,9 @@ def conf_read():
     return list
 
 
-# 传入文件绝对路径，返回迭代器
-def split_file(file):
-    try:
-        with open(file, encoding="gb18030")as f:
-            yield f.read()
-    except UnicodeDecodeError:
-        print("编码不合规:{}".format(os.path.splitext(os.path.basename(file))[0]))
-
-
-# 传入字符串，正则匹配确认字符串中是否有关键字，有则返回True
-def pipei(ziduan, guize):
-    # 遍历正则,任何一个未匹配上返回False
-    for i in guize:
-        if not re.search(i, ziduan):
-            return False
-    return True
-
-
-# 传入字符串1和字符串2，如果重复返回True
-def chongfujiancha(file_read, file_write):
-    global chongfu_count
-    chongfucanshu = 10
-    if file_read[500:500 + chongfucanshu] in file_write:
-        if file_read[300:300 + chongfucanshu] in file_write:
-            if file_read[400:400 + chongfucanshu] in file_write:
-                chongfu_count += 1
-                return True
-    return False
-
-
-# 传入绝对路径，正则匹配确认文件中是否有关键字，有则返回True
-def file_pipei(file, guize):
-    for i in split_file(file):
-        if pipei(i, guize):
-            return True
-    # 一个文件整个遍历完，没有匹配上，则返回Flase
-    return False
-
-
-# 传入绝对路径，满足规则，且不满足规则not，返回True
-def file_OK(file, guize, guize_not):
-    # 文件是否匹配需要的关键字，没匹配上返回Flase
-    if file_pipei(file, guize):
-        # 如果排除规则里有内容，则需要匹配，全部未匹配上，返回True，匹配上了返回False
-        # 如果没内容，则直接返回True
-        if guize_not:
-            # 规则匹配上，则返回True
-            if not file_pipei(file, guize_not):
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
-
-
-# 确定文件是否符合规则
-def is_file_math(file, guizes):
-    # 遍历规则，如果文件符合任何一条规则则返回真
-    for guize in guizes:
-        if fnmatch.fnmatch(file, guize):
-            return True
-    # 遍历完成后，返回假
-    # 如果前面已经返回真了，由于只能返回一个值，则这个返回假就没用了
-    return False
-
+# --------------------找出符合后缀名及大小的文件-------------------------
+# --------------------找出符合后缀名及大小的文件-------------------------
+# --------------------找出符合后缀名及大小的文件-------------------------
 
 # 获取目录及规则，返回生成器，生成器内容为
 # 目录下所有满足规则的文件的绝对路径
@@ -125,11 +65,124 @@ def size_fuhe(file):
     return False
 
 
+# 确定文内容件是否符合规则
+def is_file_math(file, guizes):
+    # 遍历规则，如果文件符合任何一条规则则返回真
+    for guize in guizes:
+        if fnmatch.fnmatch(file, guize):
+            return True
+    # 遍历完成后，返回假
+    # 如果前面已经返回真了，由于只能返回一个值，则这个返回假就没用了
+    return False
+
+
+# --------------------找出符合内容关键字的文件-------------------------
+# --------------------找出符合内容关键字的文件-------------------------
+# --------------------找出符合内容关键字的文件-------------------------
+
+# 传入文件绝对路径，满足需要规则且不满足排除规则，返回True
+def file_OK(file, guize, guize_not):
+    # 文件是否匹配需要的关键字，没匹配上返回Flase
+    if file_pipei(file, guize, ">="):
+        # 如果排除规则里有内容，则需要匹配，全部未匹配上，返回True，匹配上了返回False
+        # 如果没内容，则直接返回True
+        if guize_not:
+            # 排除的关键字
+            if file_pipei(file, guize_not, "<"):
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+
+
+# 传入文件绝对路径和需要规则列表，如果大于等于阈值，则返回True
+def file_pipei(file, guize, fuhao):
+    # 满足规则次数
+    count_manzu = 0
+    # 文件大小
+    filesize = os.path.getsize(file) / 1024
+    # 读取文件对象
+    for i in split_file(file):
+        # 遍历配置文件选项
+        l=-1
+        for j in guize:
+            l+=1
+            # 计算需要的阈值
+            count_xuyao = math.ceil(filesize / 10 * int(yuzhi[l]))
+            # 配置文件选项内容按逗号分隔，生成列表
+            guanjianzilist = j.split(",")
+            count_1 = 0
+            # 遍历配置文件选项内容列表
+            for k in range(len(guanjianzilist)):
+                # 计算文件中有多少文件选项内容列表中分词
+                count_1 += guanjianci_count(i, guanjianzilist[k])
+                # print("count_1=",count_1)
+            if fuhao == ">=":
+                if count_1 >= count_xuyao:
+                    count_manzu += 1
+            elif fuhao == "<":
+                if count_1 < count_xuyao:
+                    count_manzu += 1
+
+        # print(count_manzu, len(guize))
+        if not count_manzu == len(guize):
+            return False
+        return True
+
+
+# 传入文件绝对路径和规则列表，计算文件中有多少关键字
+def guanjianci_count(fileduixiang, fenguize):
+    k = 0
+    k += fileduixiang.count(fenguize)
+    return k
+
+
+# 传入字符串，正则匹配确认字符串中是否有关键字，有则返回True
+def pipei(ziduan, guize):
+    # 遍历正则,任何一个未匹配上返回False
+    for i in guize:
+        if not re.search(i, ziduan):
+            return False
+    return True
+
+
+# --------------------找出即将重复写入文件-------------------------
+# 传入字符串1和字符串2，如果1在2内重复，则返回True
+def chongfujiancha(file_read, file_write):
+    global chongfu_count
+    chongfucanshu = 10
+    if file_read[500:500 + chongfucanshu] in file_write:
+        if file_read[300:300 + chongfucanshu] in file_write:
+            if file_read[400:400 + chongfucanshu] in file_write:
+                chongfu_count += 1
+                return True
+    return False
+
+
+# --------------------基本函数-------------------------
+# --------------------基本函数-------------------------
+# --------------------基本函数-------------------------
+# 传入文件绝对路径，返回文件对象迭代器
+def split_file(file):
+    try:
+        with open(file, encoding="gb18030")as f:
+            yield f.read()
+    except UnicodeDecodeError:
+        print("编码不合规:{}".format(os.path.splitext(os.path.basename(file))[0]))
+
+
 # 读取配置文件
 peizhiwenjian = conf_read()
 mulu = peizhiwenjian[0][0]
 guanjianzi = peizhiwenjian[1]
+yuzhi = peizhiwenjian[4]
 guanjianzi_not = peizhiwenjian[2]
+yuzhi_pingbi = peizhiwenjian[5]
 min_size = int(peizhiwenjian[3][0])
 max_size = int(peizhiwenjian[3][1])
 
@@ -138,27 +191,29 @@ all_file = 0  # 一共有多少txt的文件
 pipeidao_file = 0  # 一共有多少关键字文件
 chongfu_count = 0  # 一共有多少重复文件
 
-# 先找出所有txt文件
+# 先找出所有txt文件，大小也要符合
 wenjianlist = is_special_file(mulu, guizes=["*txt"])
 # 遍历所有txt文件
 with open("#合并.txt", "wb+") as w1:
     for wenjian in wenjianlist:
+        # print(wenjian)
         all_file += 1
-        # 判定文件是否符合规则
+        # 判定文件内容是否符合关键字规则
         if file_OK(wenjian, guanjianzi, guanjianzi_not):
             # 判定准备录入的txt在合并txt内是否重复
-            # 打开匹配上的文件文件
+            # 先打开匹配上的文件文件
             with open(wenjian, "rb") as r:
                 # 读取内容
                 neirong = r.read()
+                # 合并大文件头开始查找，准备新写入的文件是否重复
                 w1.seek(0, 0)
                 if not chongfujiancha(neirong, w1.read()):
                     w1.seek(0, 2)
                     pipeidao_file += 1
-                    # 写入标题
+                    # 准备新写入的文件先写入标题
                     w1.write(("第{}章 666 ".format(pipeidao_file) + os.path.splitext(os.path.basename(wenjian))[
                         0] + "\n\n").encode('gb18030'))
-                    # 内容写入合并TXT
+                    # 准备新写入的文件再写入内容
                     w1.write(neirong)
                     # 不同文件之间空10行
                     print("合并新文件:{}".format(os.path.splitext(os.path.basename(wenjian))[0]))
@@ -167,4 +222,4 @@ with open("#合并.txt", "wb+") as w1:
                 else:
                     print("重复旧文件:{}".format(os.path.splitext(os.path.basename(wenjian))[0]))
 print("\n一共{}个文件,匹配到{}个需要文件,{}个重复文件".format(all_file, pipeidao_file, chongfu_count))
-# time.sleep(999)
+time.sleep(0)
